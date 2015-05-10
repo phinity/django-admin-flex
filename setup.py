81dbe54d763292e82b2b20479a737f1b20ba4479
Nick Catalano <nickcatal@gmail.com>
2015-05-10 02:35:29 -0500

    Adding license, updating readme and setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
+#!/usr/bin/env python
+
+import os
+import textwrap
+
 from setuptools import setup, find_packages
-readme = open('README.md').read()
-setup(name='django_admin_flex',
-      version='0.9.0',
-      author='Ben Kenobi',
-      author_email='ibben@gmail.com',
-      license='MIT',
-      description='Django Admin Optomized for Bootstrap',
-      long_description=readme,
-      packages=find_packages())
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+readme = textwrap.dedent(open(os.path.join(ROOT, 'README.rst')).read())
+
+setup(
+    name='django_admin_flex',
+    version='0.0.1',
+    author='Ben Korody',
+    author_email='ibben@gmail.com',
+    license='MIT',
+    description='Django Admin Optomized for Bootstrap',
+    long_description=readme,
+    url='https://github.com/ofa/django-admin-flex',
+    packages=find_packages()
+)
